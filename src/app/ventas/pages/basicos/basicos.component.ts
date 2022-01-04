8446f375a96ef1e6004d3f4bfeeea3a3059d0968
Iwo <iwoskater3@hotmail.com>
2022-01-04 14:21:10 +0100

    routing and components

---
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
