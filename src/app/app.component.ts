b5df3f86fdadbb2e1dbffec8a415611deea18207
Iwo <iwoskater3@hotmail.com>
2022-01-04 11:11:16 +0100

    new project config to use pipes and new concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'los friden';
+  valor: number = 1000;
+  obj: Object = {
+    nombre: 'friden',
+    edad: 28
+  };
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
+
 }
